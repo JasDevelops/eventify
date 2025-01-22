4bd64937d8884ef23d4bafe2788360a38c635133
Jas <50244053+JasDevelops@users.noreply.github.com>
2025-01-22 18:52:06 +0100

    Try to add e2e test (no success)
    Former-commit-id: fa2275c0a7534193a39ba2a50872ae1e4a08eba9

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,20 +1,5 @@
 import puppeteer from 'puppeteer';
-jest.mock('puppeteer-core', () => {
-	const originalModule = jest.requireActual('puppeteer-core');
 
-	return {
-		...originalModule,
-		NodeWebSocketTransport: {
-			create: (url, headers) => {
-				return new Promise((resolve, reject) => {
-					const ws = new WebSocket(url);
-					ws.onopen = () => resolve(new NodeWebSocketTransport(ws));
-					ws.onerror = reject;
-				});
-			},
-		},
-	};
-});
 describe('show/hide an event details', () => {
 	let browser;
 	let page;
