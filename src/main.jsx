a21ec29836ac667b1a941341255ed74f97282445
Jas <50244053+JasDevelops@users.noreply.github.com>
2025-01-23 11:53:59 +0100

    Set up Atatus

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+
 import * as atatus from 'atatus-spa';
 atatus.config('a9ff8126dfbd47bfb0e8dfc29b814ce4').install();
 
