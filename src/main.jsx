fd7aae2868e22969a553a6eb96e7f38570ef0656
Jas <50244053+JasDevelops@users.noreply.github.com>
2025-01-28 20:26:29 +0100

    Add manifest.json, service-worker.js & serviceWorkerRegistration.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 atatus.config('a9ff8126dfbd47bfb0e8dfc29b814ce4').install();
 
-createRoot(document.getElementById('root')).render(
-	<StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
 		<App />
-	</StrictMode>
+	</React.StrictMode>
 );
+serviceWorkerRegistration.register();
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,63 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: '/',
+	plugins: [
+		react(),
+		VitePWA({
+			manifest: {
+				short_name: 'Eventify',
+				name: 'Create React App Sample',
+				icons: [
+					{
+						src: 'assets/favicon.svg',
+						sizes: '48x48',
+						type: 'image/svg+xml',
+						purpose: 'maskable',
+					},
+					{
+						src: 'assets/eventify-144.png',
+						type: 'image/png',
+						sizes: '144x144',
+						purpose: 'any',
+					},
+					{
+						src: 'assets/eventify-192.png',
+						type: 'image/png',
+						sizes: '192x192',
+						purpose: 'maskable',
+					},
+					{
+						src: 'assets/eventify-512.png',
+						type: 'image/png',
+						sizes: '512x512',
+						purpose: 'maskable',
+					},
+				],
+				start_url: '.',
+				display: 'standalone',
+				theme_color: '#f9f9f9',
+				background_color: '#331832',
+			},
+			srcDir: 'src', // Update if your service-worker.js is elsewhere
+			filename: 'service-worker.js', // Ensure it's accessible in production
+			registerType: 'autoUpdate',
+			workbox: {
+				runtimeCaching: [
+					{
+						urlPattern: /\/.*\.png$/, // Example pattern for caching png images
+						handler: 'StaleWhileRevalidate',
+						options: {
+							cacheName: 'images',
+							expiration: {
+								maxEntries: 50,
+							},
+						},
+					},
+				],
+			},
+		}),
+	],
+});
